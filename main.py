cae5633c049b2c6dd41d236969a09f3d0a15b032
asheshjyotii <asheshjyotimajumdar3@gmail.com>
2026-03-08 02:09:52 +0530

    added single post, api, page with proper error code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -36,13 +36,21 @@ def home(request: Request):
     )
 
 
+@app.get("/posts/{post_id}")
+def post_page(request: Request, post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            title = post['title'][:50]
+            return templates.TemplateResponse(request,"post.html",{"post":post, "title":title})
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Post not found")
+
 @app.get("/api/posts")
 def get_posts():
     return posts
 
-@app.get("/api/post/{post_id}")
+@app.get("/api/posts/{post_id}")
 def get_post(post_id: int):
     for post in posts:
         if post.get("id") == post_id:
             return post
-    return {"message": "Post not found"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Post not found")
